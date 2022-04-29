2ad5f48dd65b0c18c9b4be6ce7b2ad7178ed0007
EddYam <edder_yamir@hotmail.com>
2022-04-28 23:55:32 -0500

    Agregando pruebas de unidad para la clase ExplorerService

---
+++ test/services/ExplorerService.test.js
@@ -0,0 +1,19 @@
+const Reader = require("../../lib/utils/Reader")
+const ExplorerService = require("../../lib/services/ExplorerService")
+
+describe("Test para la clase ExplorerService", () => {
+    const explorers = Reader.readJsonFile("test/explorers_test.json")
+    test("Obtener la lista de explorer que estan en la mision node", () => {
+        const result = ExplorerService.filterByMission(explorers, "node")
+        expect(result).toBe(2)
+    })
+    test("Obtener la cantidad de explorers que estan en node", () => {
+        const result = ExplorerService.getAmountOfExplorersByMission(explorers, "node")
+        expect(result).toBe(2)
+    })
+    test("Obtener la lista de usuarios de github de los explorers que estan en node", () => {
+        const result = ExplorerService.getExplorersUsernamesByMission(explorers, "node")
+        expect(result).not.toBeUndefined()
+        expect(result[0]).toBe("ajolonauta1")        
+    })
+})
