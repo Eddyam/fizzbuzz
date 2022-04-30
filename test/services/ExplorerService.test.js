91bd052acfa7a3a5080bb47f558f8d1fa4819f31
EddYam <edder_yamir@hotmail.com>
2022-04-29 22:52:02 -0500

    Modificando valores para validar pruebas

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -2,8 +2,8 @@ const Reader = require("../../lib/utils/Reader")
 const ExplorerService = require("../../lib/services/ExplorerService")
 
 describe("Test para la clase ExplorerService", () => {   
+    const explorers = Reader.readJsonFile("test/explorers_test.json")
     test("Obtener la lista de explorer que estan en la mision node", () => {
-        const explorers = Reader.readJsonFile("test/explorers_test.json")
         const result = ExplorerService.filterByMission(explorers, "node")
         expect(result.length).toBe(2)
         expect(result).toEqual(
@@ -15,12 +15,10 @@ describe("Test para la clase ExplorerService", () => {
         )
     })
     test("Obtener la cantidad de explorers que estan en node", () => {
-        const explorers = Reader.readJsonFile("test/explorers_test.json")
         const result = ExplorerService.getAmountOfExplorersByMission(explorers, "node")
         expect(result).toBe(2)
     })
     test("Obtener la lista de usuarios de github de los explorers que estan en node", () => {
-        const explorers = Reader.readJsonFile("test/explorers_test.json")
         const result = ExplorerService.getExplorersUsernamesByMission(explorers, "node")
         expect(result).not.toBeUndefined()
         expect(result[0]).toBe("EddYam")        
