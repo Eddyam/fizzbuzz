a622dadb5ae37ce522f431cd641e3dd0b0efa2a1
EddYam <edder_yamir@hotmail.com>
2022-04-29 23:43:16 -0500

    Arreglando formato con linter

--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -1,8 +1,8 @@
-const Reader = require("../../lib/utils/Reader") 
+const Reader = require("../../lib/utils/Reader"); 
 
 describe("Test para clase Reader", () => {
     test("1. Prueba para verificar lectura de archivo", () => {
-        const explorers = Reader.readJsonFile("test/explorers_test.json")
-        expect(explorers).toBeTruthy()
-    })
-})
+        const explorers = Reader.readJsonFile("test/explorers_test.json");
+        expect(explorers).toBeTruthy();
+    });
+});
