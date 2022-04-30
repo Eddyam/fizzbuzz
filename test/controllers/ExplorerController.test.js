7406c6eaaa65546e8bddcd0aa22759b8a2506cb9
EddYam <edder_yamir@hotmail.com>
2022-04-30 15:12:59 -0500

    Agregando pruebas para la clase ExplorerController

---
+++ test/controllers/ExplorerController.test.js
@@ -0,0 +1,26 @@
+const ExplorerController = require("../../lib/controllers/ExplorerController");
+
+describe("Pruebas de unidad para la clase ExplorerController", () => {
+    test("Obtener la lista de explorers filtrados por mission", () => {
+        const result = ExplorerController.getExplorerByMission("node");
+        expect(result.length).toBe(10);
+        expect(result).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({
+                    mission: "node"
+                })
+            ])
+        );
+    });
+
+    test("Regresa lista de usernames de los explorers filtrados por mission enviada", () => {
+        const result = ExplorerController.getExplorersUsernamesByMission("java");
+        expect(result).not.toBeUndefined();
+        expect(result[0]).toBe("ajolonauta6"); 
+    });
+
+    test("Regresa la cantidad de explorers en la mission enviada", () => {
+        const result = ExplorerController.getExplorersAmountByMission('node');
+        expect(result).toBe(10);
+    });
+});
